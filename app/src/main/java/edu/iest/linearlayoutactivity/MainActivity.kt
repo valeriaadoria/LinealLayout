8490e006e101fe48c685e83f72fa3c977c93aa5d
valeriaaadoria <valeria.doria@iest.edu.mx>
2023-02-01 18:56:53 -0600

    Terminado

---
+++ app/src/main/java/edu/iest/linearlayoutactivity/MainActivity.kt
@@ -0,0 +1,44 @@
+package edu.iest.linearlayoutactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var bnSuma : Button? = null
+    private var bnResta : Button? = null
+    private var etNumeroPrimero : EditText? = null
+    private var etNumeroSegundo : EditText? = null
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bnSuma = findViewById(R.id.bnSuma)
+        bnResta = findViewById(R.id.bnResta)
+        bnSuma?.setOnClickListener(this)
+        bnResta?.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        //Aqui entrara los clicks de las vistas
+        when(p0?.id){
+            R.id.bnSuma ->{
+                Toast.makeText(this,"Hice click en suma",Toast.LENGTH_LONG).show()
+            }
+        }
+        when(p0?.id){
+            R.id.bnResta -> {
+                Snackbar.make(p0,"Hice click en resta",Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
